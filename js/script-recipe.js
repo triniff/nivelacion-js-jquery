55e17c21a62cd053481189c33f44a9d494fa30e8
“Trinidad <tfuentesfuentealba@gmail.com>
2017-07-10 21:15:04 -0300

    Etapa 8 - Agregando y quitando clase make a botones

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
 	console.log("I'm ready >:O");
 	$(".recipe .js-menu").hide(); //SELECCIONANDO EL DIV JS-BACK DE RECIPE(RECIPE.HTML)
+
+	$(".js-show-make").click(function() {
+  		$(".page").addClass("make");
+	});
+	$(".js-show-recipe").click(function() {
+  		$(".page").removeClass("make");
+	});
+	
 })
